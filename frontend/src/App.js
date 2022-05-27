86af3bee96229f04732d63220f9fb382db512e15
palisha shakya <palishashakya@gmail.com>
2022-05-27 07:59:50 +0545

    Initialized frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+const App = () => (
+  <>
+    <Header />
+    <main style={{ minHeight: "93vh" }}></main>
+    <Footer />
+  </>
+);
+export default App;
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Nav, Navbar, NavDropdown, Container } from "react-bootstrap";
+const Header = () => {
+  return (
+    <Navbar bg="primary" expand="lg" variant="dark">
+      <Container>
+        <Navbar.Brand href="/">Stock Portfolio management</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Dashboard</Nav.Link>
+            {/* <Nav.Link href="#link">Link</Nav.Link> */}
+          </Nav>
+          <Nav className="mr-auto">
+            <NavDropdown title="Palisha Shakya" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">My Profile</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Logout</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
