5ec8722a4e8cfa50d679ea9be7644f12057d294c
palisha shakya <palishashakya@gmail.com>
2022-05-29 11:53:27 +0545

    user authenticated in backend

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,25 @@ const express = require("express");
 const dotenv = require("dotenv");
 const stocks = require("./data/stocks");
 const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 const app = express();
 dotenv.config();
 
 connectDB();
+
+app.use(express.json());
 app.get("/", (req, res) => {
   res.send("API is running");
 });
 app.get("/api/stocks", (req, res) => {
   res.json(stocks);
 });
+
+app.use("/api/auth", userRoutes);
+app.use(notFound);
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log(`Server running on port :http://localhost:${PORT}`)
