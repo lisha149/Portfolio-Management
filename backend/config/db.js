a809c9889a42e44e63e20e26e86dd1f3df8c1010
palisha shakya <palishashakya@gmail.com>
2022-05-29 10:48:52 +0545

    mongoDB connected to node

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
     console.log(`database is connected!!! ${conn.connection.host} `);
   } catch (error) {
     console.error(`Error: ${error} `);
